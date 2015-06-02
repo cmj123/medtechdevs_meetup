207ecdbb9663cfb19532fcd2d6f210fc8e91b7f6
Tomasz Mloduchowski <q@qdot.me>
2015-06-02 20:47:23 +0100

    Forgot admin - readded

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import *
+
+class SequenceAdmin(admin.ModelAdmin):
+    pass
+
+class ParameterAdmin(admin.ModelAdmin):
+    pass
+
+class MeasurementAdmin(admin.ModelAdmin):
+    pass
+
+for i in [
+    'Sequence', 
+    'Parameter',
+    'Measurement',
+    ]:
+    admin.site.register(globals()[i], globals()[i+'Admin'])
+
 # Register your models here.
+
+
