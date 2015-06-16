4e4f55dc0357a3705efa85da304ce1b0516fca9a
Hraban Luyat <hraban@0brg.net>
2015-06-16 20:11:36 +0100

    Manually apply 4e6082d

--- medtech_webtech/client_pusher/client_pusher/GuiDisplay.py
+++ medtech_webtech/client_pusher/client_pusher/GuiDisplay.py
@@ -1,14 +1,42 @@
-class GuiDisplay(object):
+import threading
+import tornado.ioloop
+import tornado.web
+
+#crap anti pattern will fix...
+txt = ''
+
+class WebHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(txt or '')
+
+class GuiDisplay(tornado.web.RequestHandler):
+    PORT = 9415
+
     def __init__(self):
-        self.txt = None
+        self.application = tornado.web.Application([
+            (r"/message", WebHandler),
+        ])
+        self.application.listen(self.PORT)
+
+        thread = threading.Thread(target=tornado.ioloop.IOLoop.current().start, args=())
+        thread.daemon = True
+        thread.start()
 
     def set_everything_fine(self):
-        if self.txt:
+        global txt
+        if txt:
             print "Everything is fine"
-            self.txt = None
+            txt = None
 
-    def set_warning(self, txt):
-        if self.txt != txt:
-            print "Warning: " + txt
-            self.txt = txt
+    def set_warning(self, warning_txt):
+        global txt
+        if txt != warning_txt:
+            print "Warning: " + warning_txt
+            txt = warning_txt
 
+if __name__ == "__main__":
+    display = GuiDisplay()
+    display.set_warning('testing')
+    display.set_warning('everything fine')
+    while(True):
+        pass
