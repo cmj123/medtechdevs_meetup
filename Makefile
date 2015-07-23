0462ed64700fddc989559332801dc8d6bbee6c70
Hraban Luyat <hraban@0brg.net>
2015-07-23 17:25:23 +0100

    Newest Chart.js (alpha)

--- Makefile
+++ Makefile
@@ -7,17 +7,25 @@ JS_TARGETS=$(subst static-src/,static/,$(TS_SOURCES:%.ts=%.js))
 HTML_SOURCES=$(wildcard medtech_api/static-src/*.html)
 HTML_TARGETS=$(subst static-src/,static/,$(HTML_SOURCES))
 
+ASSETS_SOURCES=$(wildcard medtech_api/assets/*)
+ASSETS_TARGETS=$(subst /assets/,/static/,$(ASSETS_SOURCES))
 
-.PHONY: all js html
+.PHONY: all js html assets
 
-all: js html
+all: js html assets
 
 js: $(JS_TARGETS)
+
+medtech_api/static/%.js: medtech_api/static-src/%.ts
+	tsc --out "$@" "$<"
 
 html: $(HTML_TARGETS)
 
 medtech_api/static/%.html: medtech_api/static-src/%.html
 	cp "$<" "$@"
 
-medtech_api/static/%.js: medtech_api/static-src/%.ts
-	tsc --out "$@" "$<"
+assets: $(ASSETS_TARGETS)
+
+$(ASSETS_TARGETS): medtech_api/static/%: medtech_api/assets/%
+	cp "$<" "$@"
+
--- medtech_api/static-src/main.ts
+++ medtech_api/static-src/main.ts
@@ -1,16 +1,4 @@
-// Testing the build
-function medtech_main() {
-    var i: number = 3;
-    var p = document.createElement('p');
-    p.textContent = 'TypeScript build succeeded: ' + i;
-    document.body.appendChild(p);
-}
-
-/* WIP
-
-function getdata(data) {
-    return Promise
-}
+declare var Chart;
 
 // Create an array with n shallow copies of the given element
 function repeat(elem, n) {
@@ -24,20 +12,76 @@ function repeat(elem, n) {
 function prepareData(elems) {
 }
 
-function medtech_main() {
-    var ctx = document.getElementById('mychart').getContext('2d');
+interface dataPoint {
+    x: number;
+    y: number;
+}
 
-    var data = {
-        labels: repeat('', 6),
-        datasets: [{
-            label: "O2",
-            data: [1,2,1,1,3,1]
+interface dataSet {
+    label: string;
+    data: dataPoint[];
+    borderColor: string;
+    backgroundColor: string;
+    pointBorderColor: string;
+    pointBackgroundColor: string;
+    pointBorderWidth: number;
+}
+
+function medtech_main() {
+    var canvas = <HTMLCanvasElement>document.getElementById('mychart');
+    var ctx = canvas.getContext('2d');
+
+    var scatterChartData = {
+        datasets: <dataSet[]>[{
+            label: 'O₂',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderColor: '#00f',
+            pointBorderColor: '#000',
+            pointBackgroundColor: 'rgba(0, 100, 0, 0.4)',
+            pointBorderWidth: 1,
+            data: [
+                {x: 0.1, y: 0.1},
+                {x: 0.2, y: 0.2},
+                {x: 0.3, y: 0.3},
+                {x: 0.4, y: 0.4},
+                {x: 0.5, y: 0.5}
+            ],
+        },
+        {
+            label: 'bpm',
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderColor: '#000',
+            pointBorderColor: '#000',
+            pointBackgroundColor: 'rgba(0, 100, 0, 0.4)',
+            pointBorderWidth: 1,
+            data: [
+                {x: 0.1, y: 0.5},
+                {x: 0.2, y: 0.4},
+                {x: 0.3, y: 0.3},
+                {x: 0.4, y: 0.2},
+                {x: 0.5, y: 0.1}
+            ],
         }],
     };
     var options = {
         bezierCurve: false,
         pointDot: false,
     }
-    var chart = new Chart(ctx).Line(data, options);
+    /*
+     * This uses the undocumented upcoming Scatter plot from Chart.js v2. The Chart.js repository has
+     * a sample using this chart type under /samples/scatter.html. 
+     */
+    var chart = Chart.Scatter(ctx, {
+        data: scatterChartData,
+        options: {
+            scales: {
+                xAxes: [{
+                    position: 'top',
+                    gridLines: {
+                        zeroLineColor: "#0f0",
+                    }
+                }],
+            }
+        }
+    });
 }
-*/
