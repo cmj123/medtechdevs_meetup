ded0caf6ed0b65b0fe4dc2915dfa8f6309a7d5a5
Tomasz Mloduchowski <q@qdot.me>
2015-09-19 14:20:35 +0100

    Vastly reworked version of the nonin driver.

--- nonin.py
+++ nonin.py
@@ -6,7 +6,12 @@ import requests
 from datetime import datetime, timedelta
 
 
-real = False
+real = True
+        # If real = True -> process /dev/ttyS4
+        # else -> process dump.raw
+mode8 = True
+        # if mode8 = False -> process mode 2
+        # else -> process mode8 dump (higher data rate)
 
 URL = 'http://copd.herokuapp.com/sensors/api/v1.0/measurement_bulk/'
 URL = 'http://localhost:8000/sensors/api/v1.0/measurement_bulk/'
@@ -18,7 +23,11 @@ if real:
 
     print ser
 
-    configuration = "\x02\x70\x02\x02\x08\x03"
+    if mode8:
+        configuration = "\x02\x70\x02\x02\x08\x03"
+    else:
+        configuration = "\x02\x70\x02\x02\x02\x03"
+
 
     print "about to write configuration", configuration
     print ':'.join(x.encode('hex') for x in configuration)
@@ -33,11 +42,15 @@ if real:
         return datetime.now().isoformat()
 
 else:
-    stime = datetime(2015, 6, 20, 23, 30, 00, 0) 
+    stime = datetime(2015, 6, 29, 23, 30, 00, 0) 
     ser = open('dump.raw', 'r')
 
-    def get_ts():
-        return (stime + timedelta(seconds=(ser.tell() / 4))).isoformat()
+    if mode8:
+        def get_ts():
+            return (stime + timedelta(microseconds=(ser.tell() * 1000 / 5 / 75))).isoformat()
+    else:
+        def get_ts():
+            return (stime + timedelta(seconds=(ser.tell() / 4))).isoformat()
 
 
 sequence  = 1
@@ -70,50 +83,134 @@ state[2] = {
     'unit': param_map[2][1],
 }
 
-print state[1]
+# print state[1]
 
 cnt = 0
+scnt = 0
+
+prev_pulse = 0  
+prev_cnt   = 0
+pp_sync = False
+
+spa = 0
+
+def process_record(rec, val):
+    if state[rec]['value'] == val:
+        state[rec]['timestamp_end'] = get_ts()
+    else:
+        if state[rec]['value'] is not None:
+            submit.append(copy.deepcopy(state[rec]))
+        state[rec]['timestamp_start'] = get_ts()
+        state[rec]['timestamp_end']   = get_ts()
+        state[rec]['value'] = val
+
+
+def submit_data():
+    for rec in (1, 2,):
+        submit.append(copy.deepcopy(state[rec]))
+
+        state[rec]['timestamp_start'] = get_ts()
+        state[rec]['timestamp_start'] = get_ts()
+        state[rec]['value'] = None
+
+    print submit
+
+    r = requests.post(
+        URL, 
+        data=json.dumps(submit), 
+        headers={'Content-type':'application/json'}
+    )
+
+    submit = []
+    print r, r.content
 
 while True:
-    print "attempting to read"
-    s = ser.read(4)
+    # print "attempting to read"
+
+    if mode8:
+        s = ser.read(5)
+    else:
+        s = ser.read(4)
     cnt += 1 
+    scnt += 1
     print ':'.join(x.encode('hex') for x in s)
 
-    if ord(s[0]) == 128:
-        print 'VALID', ord(s[1]), ord(s[2])
 
-        for rec in (1, 2,):
-            val = ord(s[rec])
+    if mode8:
+        pleth = ord(s[1]) * 256 + ord(s[2])
+    
+        pulse = (ord(s[0]) & 0x06) / 2
 
-            if state[rec]['value'] == val:
-                state[rec]['timestamp_end'] = get_ts()
-            else:
-                if state[rec]['value'] is not None:
-                    submit.append(copy.deepcopy(state[rec]))
-                state[rec]['timestamp_start'] = get_ts()
-                state[rec]['timestamp_end']   = get_ts()
-                state[rec]['value'] = val
+        snsa = ord(s[0]) & 0x70
 
-        # if len(submit) > 60*10:
-        if cnt > 600:
-            cnt = 0
+        sync = ord(s[0]) & 0x01
+
+        if sync:
+            scnt = 0
+
+        if snsa == 0:
+            if scnt == 0:
+                process_record(1, hr)
+                process_record(2, spo2)
+
+                new_hr = ord(s[3]) * 256
+
+            if scnt == 1:
+                new_hr += ord(s[3])
+                hr = new_hr 
+
+            if scnt == 2:
+                spo2 = ord(s[3])
+
+            if scnt == 7:
+                stat2 = ord(s[3])
+                spa = (stat2 & 0x20) / 0x20  
+
+            # print cnt, pleth, pulse
+
+            if pulse > 1:
+                prev_cnt = cnt
+                pp_sync = False
+
+            if pulse == 1 and prev_pulse == 0:
+                nonin_pp = 75.0/(float(hr) / 60)
+
+                
+                if (cnt - prev_cnt) < (nonin_pp * 0.5) or (cnt - prev_cnt) > (nonin_pp * 1.5):
+                    pass # Sorry, fake 
+
+                else:                
+                    if pp_sync:
+                        pass
+                        # print float(cnt - prev_cnt) / 75.0
+                        if spa: 
+                            print cnt, 60 * 75 / float(cnt - prev_cnt), nonin_pp, hr
+
+                prev_pulse = pulse
+                prev_cnt   = cnt
+
+            if prev_pulse == 1 and pulse == 0:
+                prev_pulse = pulse
+
+                pp_sync = True
+
+
+        else:
+            prev_cnt = cnt
+            pp_sync = False
+
+        if (cnt % (600 * 75)) == 0:
+            submit_data()
+
+
+    else:
+        if ord(s[0]) == 128:
+            print 'VALID', ord(s[1]), ord(s[2])
+
             for rec in (1, 2,):
-                submit.append(copy.deepcopy(state[rec]))
+                val = ord(s[rec])
+                process_record(rec, val)
 
-                state[rec]['timestamp_start'] = get_ts()
-                state[rec]['timestamp_start'] = get_ts()
-                state[rec]['value'] = None
-
-
-
-            print submit
-
-            r = requests.post(
-                URL, 
-                data=json.dumps(submit), 
-                headers={'Content-type':'application/json'}
-            )
-
-            submit = []
-            print r, r.content
+            # if len(submit) > 60*10:
+            if cnt % 600 == 0:
+                submit_data()
